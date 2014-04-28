1dde57b0a8a4ad355edf2450ea9902a4caef4bce
xuyu <xuyu16@gmail.com>
2014-04-28 23:38:45 +0800

    build with build.py

---
+++ build.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import os
+import glob
+import platform
+import sys
+import re
+
+
+package = "Pmd"
+need_prototype = ("login_command",)
+
+name = os.path.abspath(__file__)
+path = os.path.dirname(name)
+dirname = os.path.basename(path)
+gopath = os.getenv("GOPATH")
+system = platform.system().lower()
+suffix = system == "windows" and "windows.exe" or system
+
+for f in glob.glob("*.pb.go"):
+    print("rm %s" % f)
+    os.remove(f)
+
+if len(sys.argv) == 2 and sys.argv[1] == "clean":
+    sys.exit(0)
+
+dota = "%s/pkg/%s_*/git.code4.in/mobilegameserver/%s.a" % (gopath, system, dirname)
+for f in glob.glob(dota):
+    print("rm %s" % f)
+    os.remove(f)
+
+tools = os.path.join(gopath, "src/git.code4.in/mobilegameserver/tools")
+protoc = os.path.join(tools, "protoc-" + suffix)
+protocgengo = os.path.join(tools, "protoc-gen-go-" + suffix)
+for f in glob.glob("*.proto"):
+    cmd = "%s --plugin=protoc-gen-go=%s --go_out=. %s" % (protoc, protocgengo, f)
+    print(cmd)
+    os.system(cmd)
+
+for na in glob.glob("*.pb.go"):
+    fp = open(na)
+    data = fp.read()
+    fp.close()
+    regexp = re.compile("^import\s+(%s[0-9]+)" % package, re.M)
+    m = regexp.search(data)
+    if m:
+        print("fix proto import: %s" % na)
+        name = m.group(1)
+        data = []
+        fp = open(na)
+        for line in fp:
+            if not (line.startswith("import") and name in line):
+                data.append(line)
+        fp.close()
+        fp = open(na, "w")
+        fp.write("".join(data).replace(name + ".", ""))
+        fp.close()
+
+for f in need_prototype:
+    cmd = "%s %s.pb.go" % (os.path.join(tools, "prototype-" + suffix), f)
+    print(cmd)
+    os.system(cmd)
+
+cmd = "%s %s ." % (os.path.join(tools, "enummap-" + suffix), package)
+print(cmd)
+os.system(cmd)
+
+cmd = "gofmt -w -s enummap.pb.go"
+print(cmd)
+os.system(cmd)
--- proto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sed_del_import="/import.*\"$1\"/d"
-sed_del_name="s/Pmd[123456789]\.//g"
-sed -e "$sed_del_import" -e "$sed_del_name" "$2" > ../.temp.pb.go
-mv ../.temp.pb.go "$2"
-
