5310ec5a10ac21ec9649c02b530c8367bd5fedd4
wanghaijun <81681865@qq.com>
2014-08-21 20:37:27 +0800

    平台代码整理

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ import re
 
 
 package = "Pmd"
-need_prototype = ("nullcommand", "logincommand","loginjsoncommand",)
+need_prototype = ("nullcommand", "logincommand",)
 
 name = os.path.abspath(__file__)
 path = os.path.dirname(name)
