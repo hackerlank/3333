2fadbfb895576eae4149aa3377eb677efe528cea
wanghaijun <wanghaijun@ztgame.com>
2015-08-26 10:11:52 +0800

    同时支持tcp和http的混合发送

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ import re
 
 
 package = "Pmd"
-need_prototype = ("nullcommand", "logincommand","forwardcommand","sdkcommand",)
+need_prototype = ("nullcommand", "logincommand","forwardcommand","sdkcommand","chatcommand",)
 
 name = os.path.abspath(__file__)
 path = os.path.dirname(name)
