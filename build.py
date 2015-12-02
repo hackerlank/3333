acef3f8dcfd1273723f9605f4d3ddec24f2f5c89
wanghaijun <wanghaijun@ztgame.com>
2015-12-02 18:17:35 +0800

    build.py支持-c输出cpp格式

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ import glob
 import platform
 import sys
 import re
+import argparse
 
 
 package = "Pmd"
@@ -74,4 +75,45 @@ def build_go():
     print(cmd)
     assert(os.system(cmd) == 0)
 
-build_go()
+def build_cpp():
+    for f in glob.glob("*.pb.go"):
+        print("rm %s" % f)
+        os.remove(f)
+    for f in glob.glob("*.pb.h"):
+        print("rm %s" % f)
+        os.remove(f)
+    for f in glob.glob("*.pb.cc"):
+        print("rm %s" % f)
+        os.remove(f)
+    
+    if len(sys.argv) == 2 and sys.argv[1] == "clean":
+        sys.exit(0)
+    
+    dota = "%s/pkg/%s_*/git.code4.in/mobilegameserver/%s.a" % (gopath, system, dirname)
+    for f in glob.glob(dota):
+        print("rm %s" % f)
+        os.remove(f)
+    
+    tools = os.path.join(gopath, "src/git.code4.in/mobilegameserver/tools")
+    protoc = os.path.join(tools, "protoc-" + suffix)
+    for f in glob.glob("*.proto"):
+        cmd = "%s  --cpp_out=. %s  --proto_path=%s   --proto_path=%s" % (protoc, f,".","../platcommon/")
+        print(cmd)
+        assert(os.system(cmd) == 0)
+        #cmd = "%s  --cpp_out=. %s  --proto_path=%s   --proto_path=%s" % (protoc, f,".","../platcommon/")
+        #print(cmd)
+        #assert(os.system(cmd) == 0)
+
+#build_go()
+
+prog = os.path.basename(os.path.abspath(__file__))
+parser = argparse.ArgumentParser(prog="./%s" % prog)
+parser.add_argument("-c", "--cpp", action="store_true", dest="cpp", help="init build environment")
+parser.add_argument("-g", "--go", action="store_true", dest="go", help="update dependent repos")
+opts = parser.parse_args()
+if opts.cpp:
+    build_cpp()
+elif opts.go:
+    build_go()
+else:
+    build_go()
