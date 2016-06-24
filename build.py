aab9e80a5518eaa544415d79fd79c8feb9482874
wanghaijun <wanghaijun@ztgame.com>
2016-06-24 18:25:48 +0800

    增加日志服务器消息

--- build.py
+++ build.py
@@ -14,7 +14,7 @@ MD5_DICT = {}
 MD5_FILE = ".md5.pkl"
 
 package = "Pmd"
-need_prototype = ("nullcommand", "logincommand","forwardcommand","sdkcommand","monitorpmd","chatcommand","gmcommand","pushpmd","lobbypmd",)
+need_prototype = ("nullcommand", "logincommand","forwardcommand","sdkcommand","monitorpmd","chatcommand","gmcommand","pushpmd","lobbypmd","loggercommand",)
 
 name = os.path.abspath(__file__)
 path = os.path.dirname(name)
